964d84519f82b6cfeca3aa06b4922c7574986618
SamalehZen <samaleh20177@gmail.com>
2025-05-05 14:22:39 +0300

    large-testimonial.tsx

--- components/large-testimonial.tsx
+++ components/large-testimonial.tsx
@@ -33,7 +33,7 @@ export default function LargeTestimonial() {
               “Simple has simplified my life in more ways than one. From
               managing my sites to{" "}
               <em className="italic text-gray-500">Samaleh Arka Zen</em>,
-              it's become my go-to tool for everything.”
+              it's become my go-to tool for evry Abanalka.”
             </p>
             <div className="text-sm font-medium text-gray-500">
               <span className="text-gray-700">Mary Sullivan</span>{" "}
